95009767f160bf8a1e7594e5a95e307311d27f6b
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2024-06-03 18:33:47 -0700

    update query decomposition

--- test/query_decompose.py
+++ test/query_decompose.py
@@ -1,17 +1,20 @@
 import getpass
 import os
+import json
 
-path = '/Users/yiminglin/Documents/Codebase/config/openai/config.txt'
+def read_questions(file_path):
 
-def read_key(path):
-    with open(path, 'r') as file:
-    # Iterate over each line in the file
-        content = file.read()
-        lines = content.split('\n')
-    return lines[0]
+    # Open the file and load the data
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    q = []
+    sql = []
+    for item in data:
+        q.append(item.get('question'))
+        sql.append(item.get('SQL'))
+    return q, sql
 
-api_key = read_key(path)
-os.environ["OPENAI_API_KEY"] = api_key
+api_key = os.getenv('OPENAI_API_KEY')
 
 # Optional, uncomment to trace runs with LangSmith. Sign up here: https://smith.langchain.com.
 # os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -24,39 +27,72 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 
 
 class SubQuery(BaseModel):
-    """Search over a database of tutorial videos about a software library."""
+    """Search over a document collections of civic agenda projects."""
 
     sub_query: str = Field(
         ...,
-        description="A very specific query against the database.",
+        description="A very specific question against the projects.",
     )
 
 from langchain.output_parsers import PydanticToolsParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
-system = """You are an expert at converting user questions into database queries. \
-You have access to a database of tutorial videos about a software library for building LLM-powered applications. \
+def question_decomposition(question):
+    system = """You are an expert at converting user questions into  sub-questions. \
 
-Perform query decomposition. Given a user question, break it down into distinct sub questions that \
-you need to answer in order to answer the original question.
+    Perform query decomposition. Given a user question, break it down into distinct sub questions that \
+    you need to answer in order to answer the original question.
 
-If there are acronyms or words you are not familiar with, do not try to rephrase them."""
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system),
-        ("human", "{question}"),
-    ]
-)
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-llm_with_tools = llm.bind_tools([SubQuery])
-parser = PydanticToolsParser(tools=[SubQuery])
-query_analyzer = prompt | llm_with_tools | parser
+    If there are acronyms or words you are not familiar with, do not try to rephrase them."""
 
-l = query_analyzer.invoke(
-    {
-        "question": "what are the top-3 movies whose actor is john and released after 2020"
-    }
-)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            ("human", "{question}"),
+        ]
+    )
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    llm_with_tools = llm.bind_tools([SubQuery])
+    parser = PydanticToolsParser(tools=[SubQuery])
+    query_analyzer = prompt | llm_with_tools | parser
+    l = query_analyzer.invoke(
+        {
+            "question": question
+        }
+    )   
+    print(str(l))
 
-print(l)
+    return str(l)
+
+def write_json(data, file_path):
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+
+def sample_questions():
+    q = []
+    q.append('What is the number of disaster projects starting later than 2021?')
+
+    return q 
+
+if __name__ == "__main__":
+
+    #q, sql = read_questions('/Users/yiminglin/Documents/Codebase/provenance/data/questions/q_text_sql.json')
+    q = sample_questions()
+    output = []
+    for i in range(len(q)):
+        result = {}
+        question = q[i]
+        #SQL = sql[i]
+        print(i)
+        #print(question)
+        subq = question_decomposition(question)
+        result['question'] = question
+        result['subquestions'] = subq
+        output.append(result)
+        #print(SQL)
+        if(i>=5):
+            break
+    
+    write_json(output, '/Users/yiminglin/Documents/Codebase/provenance/data/questions/sample_output.json')
+    
