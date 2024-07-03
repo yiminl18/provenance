b1b5ef5ab1e767da9f289ea17398ffa0af8fafdc
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2024-07-03 12:51:45 -0700

    add debugger

---
+++ debug/evaluator.py
@@ -0,0 +1,10 @@
+#measure if summary contains ``sentence'' info, return 0(no) or 1(yes), and also proof (the closest sentences in summary)
+def summary_evaluator(summary, sentence):
+    a=0
+
+
+if __name__ == "__main__":
+    summary = ''
+    sentence = ''
+    result, proof = summary_evaluator(summary, sentence)
+    print(result, proof)
