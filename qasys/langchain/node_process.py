485b3a64d5640f4dd71d63a7839c47b23ff892d5
ZhuJuneray <juneray2003@gmail.com>
2024-11-15 20:54:35 +0000

    Remove qasys from version control and update .gitignore

--- qasys/langchain/node_process.py
+++
@@ -1,3 +0,0 @@
-def next_node(node_info, new_node):
-    node_info[f"node_{len(node_info)+1}"] = new_node
-    return node_info
--- qasys/langchain/test_batch.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-batch_input_file = client.files.create(
-  file=open("batchinput.jsonl", "rb"),
-  purpose="batch"
-)
